53d150fe227e5a58402509a3b1eece09de936e49
castletaste <savvamorozo@yandex.com>
2021-03-26 16:37:59 +0300

    add streamtransformer for items to stream_bloc.dart

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -5,14 +5,35 @@ import '../resources/repository.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
 
+  Stream<Map<int, Future<ItemModel>>> items;
+
+  StoriesBloc() {
+    items = _items.transform(_itemsTransoformer());
+  }
+  //ids getter
   Stream<List<int>> get topIds => _topIds.stream;
+  //items getter
+  Function(int) get fetchItem => _items.add;
+
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  _itemsTransoformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
+
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
